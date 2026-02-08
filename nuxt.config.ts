91ad56e80ff1419c367fdfdffb060bf7bba77238
Ben Wozniak <benrwozniak@gmail.com>
2026-02-08 10:11:39 -0600

    change favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,13 @@ import env from "./lib/env";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        { rel: "icon", type: "image/svg+xml", href: "/TablerHomeCog.svg" },
+      ],
+    },
+  },
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
